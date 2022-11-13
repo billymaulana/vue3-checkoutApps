4d6a45087db3adafaa66b8170162f2da1a853a69
Billy Maulana <billymaulana1999@gmail.com>
2022-11-13 15:23:21 +0700

    refactor: refactor store to optionApi

--- src/stores/checkout.ts
+++ src/stores/checkout.ts
@@ -1,28 +1,29 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import type { ICountry } from '../../types'
-export const useCheckoutStore = defineStore('checkout', () => {
-  const listCountries = ref()
-  const listCountriesStorage = localStorage.getItem('country')
-  if (listCountriesStorage)
-    listCountries.value = listCountriesStorage
 
-  async function fetchAllCountry() {
-    if (!listCountriesStorage) {
-      const response = useFetch<ICountry>('https://restcountries.com/v2/all?fields=name', {
-      })
-      listCountries.value = await response
-      localStorage.setItem('country', JSON.stringify(listCountries.value))
-      return response
-    }
-    else {
-      listCountries.value = JSON.parse(listCountriesStorage)
-      return listCountriesStorage
-    }
-  }
-  return {
-    listCountries,
-    fetchAllCountry,
-  }
+interface State {
+  listCountries: ICountry[]
+}
+
+const listCountriesStorage = localStorage.getItem('country')
+export const useCheckoutStore = defineStore('checkout', {
+  state: () => ({
+    listCountries: [],
+  } as unknown as State),
+  actions: {
+    async fetchAllCountry() {
+      if (!listCountriesStorage) {
+        const response = useFetch('https://restcountries.com/v2/all?fields=name')
+        this.listCountries = await response
+        localStorage.setItem('country', JSON.stringify(this.listCountries))
+        return response
+      }
+      else {
+        this.listCountries = JSON.parse(listCountriesStorage)
+      }
+    },
+  },
+  getters: {},
 })
 
 if (import.meta.hot)
--- src/stores/products.ts
+++ src/stores/products.ts
@@ -1,25 +1,55 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import type { IProduct } from 'types'
-export const useProductStore = defineStore('product', () => {
-  const product = ref({})
-  const listProducts = ref([])
+interface State {
+  listProducts: IProduct[]
+  product: IProduct
+  filteredProducts: IProduct[]
+  cart: IProduct[]
+  isLoading: boolean
+}
 
-  async function fetchAllProducts() {
-    const response = await useFetch('/products')
-    listProducts.value = await response
-    return response
-  }
-  async function fetchProduct(id: string) {
-    const response = await useFetch<IProduct>(`/products/${id}`)
-    product.value = await response
-    return response
-  }
-  return {
-    listProducts,
-    product,
-    fetchAllProducts,
-    fetchProduct,
-  }
+const cart = localStorage.getItem('cart')
+
+export const useProductStore = defineStore('product', {
+  state: () => ({
+    listProducts: [],
+    product: null,
+    filteredProducts: [],
+    cart: cart ? JSON.parse(cart) : [],
+    isLoading: true,
+  } as unknown as State),
+
+  actions: {
+    async fetchAllProducts() {
+      const response = await useFetch('/products')
+      this.listProducts = await response
+      this.isLoading = false
+      return response
+    },
+    async fetchProduct(id: string) {
+      const response = await useFetch<IProduct>(`/products/${id}`)
+      this.product = await response
+      this.isLoading = false
+      return response
+    },
+    addProduct(product: IProduct) {
+      const found = this.cart.find(item => item.id === product.id)
+      if (!found)
+        this.cart.push({ ...product, rating: { count: 1, rate: product.rating.rate } })
+      else
+        found.rating.count++
+      localStorage.setItem('cart', JSON.stringify(this.cart))
+    },
+  },
+
+  getters: {
+    getTotalPrice(state) {
+      return state.cart.reduce((totalPrice, product) => Math.round(totalPrice += product.price * product.rating.count), 0)
+    },
+    getQuantityProducts(state) {
+      return state.cart.reduce((totalQuantity, product) => totalQuantity += product.rating.count, 0)
+    },
+  },
 })
 
 if (import.meta.hot)
