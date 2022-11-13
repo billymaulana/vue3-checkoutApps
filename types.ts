0bebb0be243109a5d550b3b332a06bf7159aaa12
Billy Maulana <billymaulana1999@gmail.com>
2022-11-13 12:42:14 +0700

    chore: register interface product and country

---
+++ types.ts
@@ -0,0 +1,15 @@
+export interface IProduct {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating: {
+    rate: number
+    count: number
+  }
+}
+export interface ICountry {
+  name: 'string'
+}
