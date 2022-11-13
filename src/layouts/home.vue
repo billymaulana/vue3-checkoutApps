356461e29ab53cbd3d99e94beb800cc38291d549
Billy Maulana <billymaulana1999@gmail.com>
2022-11-13 12:43:12 +0700

    feat: layout home

---
+++ src/layouts/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar />
+  <Suspense>
+    <template #fallback>
+      <div h-screen w-screen flex items-center justify-center>
+        <Loading />
+      </div>
+    </template>
+    <template #default>
+      <main dark:text-white>
+        <router-view />
+      </main>
+    </template>
+  </Suspense>
+</template>
