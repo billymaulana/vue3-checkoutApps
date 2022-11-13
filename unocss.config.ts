b165e1773f92b719dba462c50c215dd3b0581d1e
Billy Maulana <billymaulana1999@gmail.com>
2022-11-13 12:50:48 +0700

    chore: config rules and shortcut unocss

--- unocss.config.ts
+++ unocss.config.ts
@@ -9,8 +9,32 @@ import {
 } from 'unocss'
 
 export default defineConfig({
+  rules: [
+    [/^f-(\d+)$/, ([, d]) => ({ 'font-size': `${d}px` })],
+    [/^anim-delay-(\d+)$/, ([, d]) => ({ 'animation-delay': `${d}s` })],
+  ],
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['container-fluid', 'max-w-[100%'],
+    ['row', 'flex flex-wrap'],
+    ['col-auto', 'flex-[0_0_auto] w-auto'],
+    ['col-1', 'flex-[0_0_auto] w-[8.3333333333%]'],
+    ['col-2', 'flex-[0_0_auto] w-[16.6666666667%]'],
+    ['col-3', 'flex-[0_0_auto] w-[25%]'],
+    ['col-4', 'flex-[0_0_auto] w-[33.3333333333%]'],
+    ['col-5', 'flex-[0_0_auto] w-[41.6666666667%]'],
+    ['col-6', 'flex-[0_0_auto] w-[50%]'],
+    ['col-7', 'flex-[0_0_auto] w-[58.3333333333%]'],
+    ['col-8', 'flex-[0_0_auto] w-[66.6666666667%]'],
+    ['col-9', 'flex-[0_0_auto] w-[75%]'],
+    ['col-10', 'flex-[0_0_auto] w-[83.3333333333%]'],
+    ['col-11', 'flex-[0_0_auto] w-[91.6666666667%]'],
+    ['col-12', 'flex-[0_0_auto] w-[100%]'],
+    ['w-50', 'w-[50%]'],
+    ['shadow-left', 'shadow-[0 0 0 1px #e0e0e0, 0 2px 4px 0 rgb(0 0 0 / 7%), 0 1px 1.5px 0 rgb(0 0 0 / 5%)]'],
+    ['shadow', 'shadow-[15px_0_30px_0_rgba(0,0,0,0,5)]'],
+    ['form-group-merge', 'rounded-lg overflow-hidden border-width-[1px] border-gray-200 shadow-md rounded-lg overflow-hidden border-width-[1px] border-gray-200 shadow-md'],
+    ['btn-primary', 'bg-black rounded-[6px] text-white h-[44px] w-full flex justify-center items-center font-[18px] font-bold'],
+    ['filter-bw', 'filter-[grayscale(1)_saturate(0)_contrast(1)]'],
   ],
   presets: [
     presetWind(),
@@ -30,12 +54,25 @@ export default defineConfig({
   ],
   theme: {
     colors: {
-      veryCool: '#0000ff', // class="text-very-cool"
-      primary: 'hsla(var(--hue, 217), 78%, 51%)', // class="bg-primary"
+      dark: '#1e232b',
+      darksecondary: '#18181b',
+      dimgrey: '#292e36',
+      black: '#303030',
+      lightgray: '#f0f0f0',
+      gray: '#767676',
+      gray2: '#5e5e5e',
+      red: '#dc2727',
+    },
+    container: {
+      center: true,
     },
     breakpoints: {
-      sm: '320px',
-      md: '640px',
+      'xs': '1px',
+      'sm': '320px',
+      'md': '768px',
+      'lg': '996px',
+      'xl': '1440px',
+      '2xl': '1536px',
     },
   },
   transformers: [
