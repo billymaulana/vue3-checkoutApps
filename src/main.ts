3e7df3f760d247de08e67b8e6007b7512c509a80
Billy Maulana <billymaulana1999@gmail.com>
2022-11-13 12:51:27 +0700

    chore: register masking plugins

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
+import Maska from 'maska'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
-import './assets/styles/main.css'
 import 'uno.css'
+import './assets/styles/main.css'
 
 const routes = setupLayouts(generatedRoutes)
 
@@ -15,5 +17,7 @@ const router = createRouter({
   routes,
 })
 const app = createApp(App)
+app.use(createPinia())
 app.use(router)
+app.use(Maska)
 app.mount('#app')
